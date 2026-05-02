fd4d0c21c4563696a4fb3d1455e07bbe2cba2ca6
hose246 <145179667+hose246@users.noreply.github.com>
2026-05-01 23:06:29 -0700

    Update BoardStyle.java
    Finished interface for the styles. I think this will be the finished code unless I decide to change a lot in the Concrete classes.

--- src/BoardStyle.java
+++ src/BoardStyle.java
@@ -1,3 +1,27 @@
-// interface
-// drawPit(Graphics g, int w, int h, int stones), called from MancalaPit
-// drawMancala(Graphics g, int w, int h, int stones), called from MancalaPit
+import java.awt.*;
+
+public interface BoardStyle {
+	/**
+	 * Draw a pit on the board.
+	 * Precondition: none
+	 * Postcondition: a pit is drawn on the mancala board
+	 * 
+	 * @param g				Graphics object
+	 * @param width			the width of the pit
+	 * @param height		the height of the pit
+	 * @param stones		the amount of stones in the pit
+	 */
+	void drawPit(Graphics g, int width, int height, int stones);
+	
+	/**
+	 * Draw a mancala on the board. (e.g. Mancala A, Mancala B. This does not draw the entire game Mancala)
+	 * Precondition: none
+	 * Postcondition: a mancala is drawn on the board
+	 * 
+	 * @param g				Graphics object
+	 * @param width			the width of the Mancala
+	 * @param height		the height of the Mancala
+	 * @param stones		the amount of stones in the Mancala
+	 */
+	void drawMancala(Graphics g, int width, int height, int stones);
+}
