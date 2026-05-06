8d1befb25b7350ad580b4acad04671c7508f8728
hose246 <145179667+hose246@users.noreply.github.com>
2026-05-06 00:07:56 -0700

    Update Style1.java

--- src/Style1.java
+++ src/Style1.java
@@ -14,7 +14,7 @@ public class Style1 implements BoardStyle {
 	 */
 	public void drawPit(Graphics g, int w, int h, int stones) {
 		Graphics2D g2 = (Graphics2D) g;
-		int padding = 15;
+		int padding = 5;
 		int size = Math.min(w,  h) - 2 * padding;
 		
 		int x = (w - size) / 2;
@@ -26,7 +26,7 @@ public class Style1 implements BoardStyle {
 		g2.setColor(Color.BLACK);
 		g2.drawOval(x,  y, size, size);
 		
-		drawStones(g2, x, y, size, stones);
+		drawStones(g2, x, y, size, size, stones);
 	}
 	
 	/**
@@ -40,10 +40,22 @@ public class Style1 implements BoardStyle {
 	 * @param stones		amount of stones in the mancala
 	 */
 	public void drawMancala(Graphics g, int w, int h, int stones) {
-		g.setColor(new Color(160, 82, 45));
-		g.fillRoundRect(5, 5, w-10, h-10, 20, 20);
+		Graphics2D g2 = (Graphics2D) g;
 		
+		int padding = 5;
 		
+		int x = padding;
+		int y = padding;
+		int width = w - 2 * padding;
+		int height = h - 2 * padding;
+		
+		g2.setColor(new Color(160, 82, 45));
+		g2.fillRoundRect(x, y, width, height, 30, 30);
+		
+		g2.setColor(Color.BLACK);
+		g2.drawRoundRect(x,  y, width, height, 30, 30);
+		
+		drawStones(g2, x, y, width, height, stones);
 	}
 	
 	/**
@@ -54,17 +66,18 @@ public class Style1 implements BoardStyle {
 	 * @param g				Graphics object
 	 * @param x				the x location of the stone
 	 * @param y				the y location of the stone
-	 * @param size			the size of the stone
+	 * @param width			the width of the pit/mancala
+	 * @param height		the height of the pit/mancala
 	 * @param stones		the amount of stones to draw
 	 */
-	public void drawStones(Graphics g, int x, int y, int size, int stones) {
+	public void drawStones(Graphics g, int x, int y, int width, int height, int stones) {
 		g.setColor(Color.BLACK);
 		
 		int radius = 8;
-		int centerX = x + size / 2;
-		int centerY = y + size / 2;
+		int centerX = x + width / 2;
+		int centerY = y + height / 2;
 		
-		int spacing = 18;
+		int spacing = Math.min(width, height) / 3;
 		
 		for(int i = 0; i < stones; i++) {
 			double angle = 2 * Math.PI * i / stones;
